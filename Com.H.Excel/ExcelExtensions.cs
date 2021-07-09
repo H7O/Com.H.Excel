01011a1b51e81e657d68968e299c22d86c42a804
Hussein Al Bayati <hussein@albayati.com>
2021-07-09 06:18:26 +0400

    Read / Parse excel

--- Com.H.Excel/ExcelExtensions.cs
+++ Com.H.Excel/ExcelExtensions.cs
@@ -12,6 +12,9 @@ using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using System.Globalization;
 using Com.H.IO;
+using System.Data;
+using System.Dynamic;
+using Com.H.Text;
 
 namespace Com.H.Excel
 {
@@ -38,18 +41,56 @@ namespace Com.H.Excel
             {typeof(DateTime), CellValues.Number },
             };
 
-        
+        private static Dictionary<CellValues, Type> TypeToOpenXmlDic { get; set; }
+        = new Dictionary<CellValues, Type>()
+        {
+            {CellValues.String, typeof(string) },
+            {CellValues.Number, typeof(decimal?)},
+            {CellValues.Boolean, typeof(bool?) },
+            {CellValues.Date, typeof(DateTime?) }
+            };
+
+
         private static CellValues GetOpenXmlType(this Type type)
         {
             if (OpenXmlTypesDic.Keys.Contains(type))
                 return
                     OpenXmlTypesDic[type];
-            
+
             return CellValues.String;
         }
 
+        private static Type GetTypeFromOpenXml(this CellValues type)
+        {
+            if (TypeToOpenXmlDic.Keys.Contains(type))
+                return
+                    TypeToOpenXmlDic[type];
+
+            return typeof(string);
+        }
+
+
         #endregion
 
+
+        #region format types
+        private enum Formats
+        {
+            General = 0,
+            Number = 1,
+            Decimal = 2,
+            Currency = 164,
+            Accounting = 44,
+            DateShort = 14,
+            DateLong = 165,
+            Time = 166,
+            Percentage = 10,
+            Fraction = 12,
+            Scientific = 11,
+            Text = 49
+        }
+
+        #endregion
 
         #region excel generation
         public static Stream ToExcelReader(
@@ -75,7 +116,7 @@ namespace Com.H.Excel
 
             var path =
                 Path.Combine(tempBasePath,
-                (string.IsNullOrEmpty(preferredTempFileName) ? 
+                (string.IsNullOrEmpty(preferredTempFileName) ?
                 Guid.NewGuid().ToString() + ".xlsx"
                 : preferredTempFileName)).EnsureParentDirectory();
 
@@ -123,7 +164,7 @@ namespace Com.H.Excel
 
         private static void AddDateStyle(this WorkbookPart workbookPart)
         {
-            Stylesheet styleSheet = new Stylesheet();
+            Stylesheet styleSheet = new();
 
             var cf1 = new CellFormat
             {
@@ -152,7 +193,7 @@ namespace Com.H.Excel
             workbookPart.AddNewPart<WorkbookStylesPart>();
             workbookPart.WorkbookStylesPart.Stylesheet = styleSheet;
 
-            CellStyles css = new CellStyles();
+            CellStyles css = new();
             var cs = new CellStyle
             {
                 FormatId = 0,
@@ -201,7 +242,7 @@ namespace Com.H.Excel
                     WorksheetPart worksheetPart = workbookpart.AddNewPart<WorksheetPart>();
                     worksheetPart.Worksheet = new Worksheet(new SheetData());
 
-                    Sheet sheet = new Sheet()
+                    Sheet sheet = new()
                     {
                         Id = spreadsheetDocument.WorkbookPart.GetIdOfPart(worksheetPart),
                         SheetId = (UInt32)sheetNumber,
@@ -295,7 +336,7 @@ namespace Com.H.Excel
                 catch { }
                 #endregion
             }
-            catch 
+            catch
             {
                 throw;
             }
@@ -305,5 +346,164 @@ namespace Com.H.Excel
 
         #endregion
 
+
+        #region read excel
+
+        public static Dictionary<string, List<dynamic>> ParseExcel(
+            this System.IO.Stream inStream, bool skipHeaders = false)
+        {
+            SpreadsheetDocument doc = SpreadsheetDocument.Open(inStream, false);
+            var excelSheets = doc?.WorkbookPart?.Workbook?
+                    .Descendants<Sheet>();
+            WorkbookPart workbookPart = doc.WorkbookPart;
+            Dictionary<string, SheetData> sheets = excelSheets.ToDictionary(
+                key => key.Name?.ToString(), value =>
+                ((WorksheetPart)workbookPart.GetPartById(value.Id))
+                    .Worksheet.GetFirstChild<SheetData>());
+
+            Dictionary<string, List<dynamic>> result = new();
+
+            foreach (var sheet in sheets)
+            {
+                result.Add(sheet.Key, new List<dynamic>());
+
+                Dictionary<string, Type> headers = skipHeaders ?
+                         Enumerable.Range(0, sheet.Value.Count() - 1)
+                        .Select(x => $"column_{x}")
+                        .ToDictionary(key => key, value => typeof(string))
+                        : sheet.Value?.FirstOrDefault()?.Select(x =>
+                         ((Cell)x).GetText(workbookPart)).ToDictionary(key => key, value => typeof(string));
+
+                List<string> headerNames = headers.Keys?.ToList();
+
+
+                foreach (Row row in sheet.Value.Skip(skipHeaders ? 0 : 1))
+                {
+                    ExpandoObject d = new();
+                    int headerIndex = -1;
+                    foreach (Cell cell in row.Select(x => (Cell)x))
+                    {
+                        headerIndex++;
+                        var headerName = headerNames[headerIndex];
+                        Type type =
+                            (headers[headerName] = cell.GetDataType(workbookPart)
+                            ?? headers[headerName]??typeof(string));
+
+                        if (cell == null)
+                        {
+                            d.TryAdd(headerName, type.GetDefault());
+                            continue;
+                        }
+
+                        int? index = cell.GetCellColIndex() - 1;
+                        if (index == null) break;
+
+                        // fill collapsed columns
+                        if (index > headerIndex)
+                            headerName = headerNames[headerIndex = Enumerable.Range(headerIndex, (int) index - headerIndex)
+                                .Aggregate(headerIndex, (i, n) =>
+                                {
+                                    d.TryAdd(headerNames[i],
+                                        (headers[headerNames[i]] = cell.GetDataType(workbookPart)
+                                        ?? headers[headerNames[i]]??typeof(string)).GetDefault());
+                                    return n+1;
+                                })];
+
+                        object value = null;
+
+                        try
+                        {
+                            value = cell.GetObject(workbookPart); // Convert.ChangeType(cell.GetText(doc), type, CultureInfo.InvariantCulture);
+                        }
+                        catch { }
+                        d.TryAdd(headerName, value ?? type.GetDefault());
+
+                    }
+                    result[sheet.Key].Add(d);
+                }
+
+
+            }
+            return result;
+        }
+        private static Type GetDataType(this Cell cell, WorkbookPart workbookPart)
+        {
+
+            if (cell?.DataType?.Value != null) return cell.DataType.Value.GetTypeFromOpenXml();
+
+            if (cell?.StyleIndex?.Value == null) return null;
+
+            int? styleIndex = (int?)cell.StyleIndex?.Value;
+            if (styleIndex == null) return null;
+            CellFormat cellFormat = (CellFormat)workbookPart.WorkbookStylesPart.Stylesheet.CellFormats.ElementAt((int)styleIndex);
+            uint formatId = cellFormat.NumberFormatId.Value;
+            return formatId switch
+            {
+                (uint)Formats.DateShort => typeof(DateTime?),
+                (uint)Formats.DateLong => typeof(DateTime?),
+                (uint)Formats.Number => typeof(decimal?),
+                (uint)Formats.Decimal => typeof(decimal?),
+                _ => null
+            };
+
+        }
+        private static string GetText(this Cell cell, WorkbookPart workbookPart)
+        {
+            string value =
+            (cell.DataType != null && cell.DataType.Value == CellValues.SharedString) ?
+                workbookPart.SharedStringTablePart.SharedStringTable.ChildElements[int.Parse(cell.CellValue.Text)].InnerText :
+                cell.CellValue.Text;
+            return value;
+        }
+
+        private static object GetObject(this Cell cell, WorkbookPart workbookPart)
+        {
+            if (cell == null) return null;
+            if (cell.DataType != null)
+            {
+                switch (cell.DataType?.Value)
+                {
+                    case CellValues.SharedString:
+                        SharedStringItem ssi = workbookPart.SharedStringTablePart.SharedStringTable.Elements<SharedStringItem>().ElementAt(int.Parse(cell.CellValue.InnerText));
+                        return ssi.Text.Text;
+                    case CellValues.Boolean:
+                        return cell.CellValue?.InnerText == "0";
+                    default:
+                        return cell.CellValue?.InnerText ?? cell.CellValue.Text;
+                }
+            }
+
+            int? styleIndex = (int?)cell.StyleIndex?.Value;
+            if (styleIndex == null) return cell.CellValue?.Text;
+            
+            CellFormat cellFormat = (CellFormat)workbookPart.WorkbookStylesPart.Stylesheet.CellFormats.ElementAt((int) styleIndex);
+            uint formatId = cellFormat.NumberFormatId.Value;
+            if (formatId == (uint)Formats.DateShort || formatId == (uint)Formats.DateLong)
+            {
+                if (double.TryParse(cell.InnerText, out double oaDate))
+                    return DateTime.FromOADate(oaDate);
+            }
+            else
+            {
+                if (decimal.TryParse(cell.InnerText, out decimal d)) return d;
+            }
+            return cell.CellValue?.Text;
+        }
+
+
+
+        /// <summary>
+        /// Extract column index from cell reference.
+        /// edited from http://stackoverflow.com/questions/848147/how-to-convert-excel-sheet-column-names-into-numbers
+        /// </summary>
+        /// <param name="cell"></param>
+        /// <returns>Cell column index</returns>
+        /// 
+        private static int? GetCellColIndex(this Cell cell)
+            => cell?.CellReference?.Value?.ExtractAlphabet()?.ToUpper()?.
+               Aggregate(0, (i, n) => 26 * i + n - 'A' + 1);
+
+
+        #endregion
     }
 }
